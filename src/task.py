27856c65104a95b5fef07ff887787a846b3c0a48
tunim73 <antonio.netto973@gmail.com>
2023-12-01 01:14:56 -0300

    código concluído

--- src/task.py
+++ src/task.py
@@ -10,12 +10,11 @@ media
 moda 
 frequencia and mediana
 
-
 """
 
 
 
-
+# Tokens foram modificados { 
 
 tokens = (
     'LOAD', 'NAME', 'EXIT', 'AND', 'MEDIA', 'MEDIANA', 'MODA', 'FREQUENCIA'
@@ -33,6 +32,10 @@ def t_NAME(t):
     r'[a-zA-Z_][a-zA-Z0-9_]*\.txt'
     return t
 
+# }
+
+
+# Não modificados {
 t_ignore = " \t"
 
 def t_newline(t):
@@ -44,12 +47,23 @@ def t_error(t):
     t.lexer.skip(1)
 
 lexer = lex.lex()
+# }
 
+
+# regras de precedências modificado {
 precedence = (
     ('left', 'AND'),
 )
+# }
+
+
+# Para armazenar o array que veio do arquivo txt
 current_array = []
 
+
+# Produções modificas { 
+
+# S -> C | E 
 def p_start(p):
     '''
     start : command
@@ -58,7 +72,7 @@ def p_start(p):
     print(p[1])
 
 
-
+# C -> LOAD NAME
 def p_command_load(p):
     'command : LOAD NAME'
     filename = p[2]
@@ -83,13 +97,14 @@ def p_command_load(p):
     except ValueError:
         print(f"Array no '{filename}' no formato incorreto. Use espaço ' ' como separador")
     
+# C -> EXIT
 def p_command_exit(p):
     'command : EXIT'
     exit()
 
 
-
 # Regra para a expressão
+# E -> F | E AND E (expresão com a recursão à esquerda, sem o não terminal G)
 def p_expression(p):
     '''
     expression : F
@@ -100,8 +115,7 @@ def p_expression(p):
     elif p[2] == 'and':
         p[0] = p[1] + p[3]
 
-
-
+# F -> MEDIA
 def p_function_media(p):
     'F : MEDIA'
     global current_array
@@ -112,6 +126,7 @@ def p_function_media(p):
     else:
         print("Sem array para utilização.")
 
+# F -> MEDIANA
 def p_function_mediana(p):
     'F : MEDIANA'
     global current_array
@@ -121,6 +136,7 @@ def p_function_mediana(p):
     else:
         print("Sem array para utilização.")
 
+# F -> MODA
 def p_function_moda(p):
     'F : MODA'
     global current_array
@@ -130,6 +146,7 @@ def p_function_moda(p):
     else:
         print("Sem array para utilização.")
 
+# F -> FREQUENCIA
 def p_function_frequencia(p):
     'F : FREQUENCIA'
     global current_array
@@ -139,9 +156,11 @@ def p_function_frequencia(p):
     else:
         print("Sem array para utilização.")
 
+# } 
 
+# Daqui para baixo, sem modificações que intereferem em algo
 
-# Regra para lidar com erros de sintaxe
+# Regra para lidar com erros de sintaxe 
 def p_error(p):
     if p:
         print(f"Syntax error at '{p.value}'")
